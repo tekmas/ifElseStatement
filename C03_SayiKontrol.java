9efc73f136c729445788a7b79514df1ebacbfe27
mehmet ali sürücü <tekmas83@gmail.com>
2022-03-01 18:20:44 +0300

    java project

---
+++ C03_SayiKontrol.java
@@ -0,0 +1,9 @@
+package day08_ifElseIfStatements;
+
+import java.util.Scanner;
+
+public class C03_SayiKontrol {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+    }
+}
---
+++ examples.java
@@ -0,0 +1,26 @@
+package day08_ifElseIfStatements;
+
+import java.util.Scanner;
+
+public class examples {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lütfen çizdiğiniz dörtgenin ardışık kenarlarından ilk kenar uzunluğunu girin");
+        double ilk_kenar = scan.nextDouble();
+        System.out.println("lütfen çizdiğiniz dörtgenin diğer kenar uzunluğunu girin");
+        double ikinci_kenar = scan.nextDouble();
+        System.out.println("lütfen çizdiğiniz dörtgenin diğer kenar uzunluğunu girin");
+        double ucuncu_kenar = scan.nextDouble();
+        System.out.println("lütfen çizdiğiniz dörtgenin diğer kenar uzunluğunu girin");
+        double dorduncu_kenar = scan.nextDouble();
+        if ((ilk_kenar == ikinci_kenar) && (ikinci_kenar == ucuncu_kenar)) {
+            System.out.println("çizilen dörtgen bir karedir");
+        }
+        else if ((ilk_kenar == ucuncu_kenar) && (ikinci_kenar == dorduncu_kenar)) {
+            System.out.println("çizilen dörtgen bir dikdörtgendir");
+        }
+        else if (ikinci_kenar != ucuncu_kenar) {
+            System.out.println("çizilen dörtgen bir yamuktur");
+        }
+    }
+}
